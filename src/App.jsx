f01087366264fc59b3875e17a7b7425492004765
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-18 18:36:23 -0500

    stared project of EasyEat

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Siderbar from './components/shared/Siderbar'
+
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Siderbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/shared/Siderbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Siderbar = () => {
+  return <div className="bg-black fixed left-0 top-0 w-28 h-">Siderbar</div>;
+};
+
+export default Siderbar;
