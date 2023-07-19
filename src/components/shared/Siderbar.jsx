64a8908125f09ec422489de33ee8d03ff10622b7
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-18 19:20:44 -0500

    add configuration dependenses of taildwind and ccs on siderbar

--- src/components/shared/Siderbar.jsx
+++ src/components/shared/Siderbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Siderbar = () => {
-  return <div className="bg-black fixed left-0 top-0 w-28 h-">Siderbar</div>;
+  return <div className="text-white bg-[#1F1D2B] fixed 
+  left-0 top-0 w-28 h-full">
+    <h1 className="text-2xl  text-grey-300 uppercase
+     font-bold text-center my-5">
+        logo
+        </h1>
+        <ul>
+            <li>
+                <a href="a"></a>
+            </li>
+        </ul>
+    </div>;
 };
 
 export default Siderbar;
