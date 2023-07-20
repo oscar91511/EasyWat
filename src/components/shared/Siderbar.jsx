152bff2e9b2a112ef2825ec98406e8ebfe86b630
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-20 15:28:27 -0500

    work on navbar corrections on container div

--- src/components/shared/Siderbar.jsx
+++ src/components/shared/Siderbar.jsx
@@ -1,48 +1,97 @@
 import React from "react";
-import { RiStore3Fill, RiPercentLine, RiPieChartLine } from "react-icons/ri";
+import {
+  RiStore3Fill,
+  RiPercentLine,
+  RiPieChartLine,
+  RiMailLine,
+  RiNotification3Line,
+  RiSettings4Fill,
+  RiLogoutCircleLine,
+} from "react-icons/ri";
 
 const Siderbar = () => {
   return (
     <div
       className="text-white bg-[#1F1D2B] fixed 
-  left-0 top-0 w-28 h-full py-4"
+  left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl"
     >
+      <div>
       <h1
         className="text-2xl  text-grey-300 uppercase
      font-bold text-center my-5"
       >
         logo
       </h1>
-      <ul className="pl-4  bg-stone-900/40">
-    
-        <li className=" bg-stone-700 p-4 rounded-tl-xl rounded-bl-xl">
-          <a
-            className=" flex justify-center bg-orange-600 rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
-            text-center"
-            href="#"
-          >
-            <RiStore3Fill className="text-2xl "/>
-          </a>
-        </li>
-        <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
-          <a
-            className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
-            text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
-            href="#"
-          >
-            <RiPercentLine className="text-2xl "/>
-          </a>
-        </li>
-        <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
-          <a
-            className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
-            text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
-            href="#"
-          >
-            <RiPieChartLine className="text-2xl "/>
-          </a>
-        </li>
-      </ul>
+        <ul className="pl-4  bg-stone-900/40">
+          <li className=" bg-stone-700 p-4 rounded-tl-xl rounded-bl-xl">
+            <a
+              className=" flex justify-center bg-orange-600 rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
+        text-center"
+              href="#"
+            >
+              <RiStore3Fill className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
+        text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
+              href="#"
+            >
+              <RiPercentLine className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
+        text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
+              href="#"
+            >
+              <RiPieChartLine className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
+        text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
+              href="#"
+            >
+              <RiMailLine className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
+        text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
+              href="#"
+            >
+              <RiNotification3Line className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
+        text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
+              href="#"
+            >
+              <RiSettings4Fill className="text-2xl " />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="pl-4  bg-stone-900/40">
+          <li className=" hover:bg-stone-700  p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              className=" flex justify-center  rounded-tl-xl rounded-bl-xl rounded-xl p-4 box-content text-3xl
+        text-center text-orange-600 group-hover:bg-orange-600 group-hover:text-white"
+              href="#"
+            >
+              <RiLogoutCircleLine className="text-2xl " />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
