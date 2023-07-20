8ad884e2fed798a676df14e81da9906213c7df9f
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-20 18:12:59 -0500

    working on component MenuOrder.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import React from "react";
 import Siderbar from "./components/Siderbar";
 import ShowMenu from "./components/ShowMenu";
+import MenuOrder from "./components/MenuOrder";
 
 const App = () => {
   return (
     <div className="bg-[#334155] w-full min-h-screen">
       <Siderbar />
       <ShowMenu />
+      <MenuOrder/>
     </div>
   );
 };
---
+++ src/components/MenuOrder.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MenuOrder = () => {
+  return (
+    <main className='lg:pl-28 grid grid-cols-1 lg:grid-cols-8'>
+        <div className='lg:col-span-6'>Hola1</div>
+        <div className='lg:col-span-2 fixed
+         lg:static right-0'>carrito</div>
+    </main>
+  )
+}
+
+export default MenuOrder
